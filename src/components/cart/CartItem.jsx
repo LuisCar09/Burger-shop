5fd2c2629f3d5c181a7fbc0a6865ad3a6fbf5f1b
Luis Carlos Bravo <luiscbravo94@gmail.com>
2024-02-21 22:01:30 +0000

    Add sendDataToPatch to send data and patch data at backend to change value of each item in backend

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,26 +1,47 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const CartItem = ({ src, alt, itemName }) => {
+const CartItem = ({ src, alt, itemName, amount, id }) => {
 
     const [number, setNumber] = useState(0)
 
-    const numberToAdd = () => {
+    const sendDataToPatch = async () => {
+        //If number equals to 1 or higher send to backend value of id and number, then at the backend we will check it out if id exist on this, if exist we will change value on backend to show us the correct data on rendering data in card item
+        if (number >= 1) {
+            await axios.patch('http://localhost:3001/add', { id, number })
+        }
+
+        
+    }
+
+
+    const numberToAdd = async () => {
         setNumber(prevValue => prevValue + 1)
 
     }
-    const numberToSubtract = () => {
 
+    const numberToSubtract = () => {
 
         setNumber((prevValue) => {
             if (prevValue <= 0) {
                 return 0
-            }else{
+            } else {
                 return prevValue - 1
             }
         })
-        
-    }
 
+    }
+    //Change number value, this value comes from confirOrdersCart, we receive this value from backend with burger's amount, then put into number variable to set it up, then this value will render on each burger to says us how many burger were chosen.
+    useEffect(() => {
+
+        setNumber(amount)
+    }, [])
+
+    //Here until number changes its value, we will call sendDataToPatch() function to send from here to backend to storage how many of each burger are.
+    useEffect(() => {
+        
+        sendDataToPatch()
+    }, [number])
 
     return (
         <article>
--- src/components/cart/ConfirmOrdersCart.jsx
+++ src/components/cart/ConfirmOrdersCart.jsx
@@ -1,35 +1,35 @@
-import {React,useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Fade from '@mui/material/Fade';
 import CartItem from "./CartItem";
-import { v4 as uuidv4 } from "uuid";
-import burger1 from '../../assets/burger1.png'
-import burger2 from '../../assets/burger2.png'
-import burger3 from '../../assets/burger3.png'
+
+// import burger1 from '../../assets/burger1.png'
+// import burger2 from '../../assets/burger2.png'
+// import burger3 from '../../assets/burger3.png'
 
 
 
 const ConfirmOrdersCart = ({ isOpenCart, isClose }) => {
-    const [counter,setCounter] = useState(0)
-    const [amount,setAmount] = useState(0)
-    const [burgers,setBurgers] = useState([])
-    const uniqueId = uuidv4();
-    const getBurgers =  async () => {
+    const [counter, setCounter] = useState(0)
+    const [amount, setAmount] = useState(0)
+    const [burgers, setBurgers] = useState([])
+
+    const getBurgers = async () => {
         try {
             const response = await axios.get('http://localhost:3001/add')
             setBurgers(response.data)
         } catch (error) {
-            console.error('Error fetching data',error);
+            console.error('Error fetching data', error);
         }
     }
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         getBurgers()
-        
-    },[])
-    
-    
+
+    }, [])
+
+
 
     //Aqui evitamos que el listener se ejecute cuando se haga click dentro del componente, de esta forma counter solo aumentara cuando el click sea fuera de este componente
     const handlerClickInsideCart = (event) => {
@@ -37,24 +37,24 @@ const ConfirmOrdersCart = ({ isOpenCart, isClose }) => {
     }
 
     //Al realizar click fuera del componente se le sumara 1 a la variable counter.
-    const handlerToOpen = () =>{
-        setCounter((prevValue) => {return(prevValue + 1)})
-        
-        
+    const handlerToOpen = () => {
+        setCounter((prevValue) => { return (prevValue + 1) })
+
+
     }
-    useEffect(()=>{
-        window.addEventListener('click',handlerToOpen)
-        return (() => {window.removeEventListener('click',handlerToOpen)})
-    },[])
-    
-    
+    useEffect(() => {
+        window.addEventListener('click', handlerToOpen)
+        return (() => { window.removeEventListener('click', handlerToOpen) })
+    }, [])
+
+
     //Utilizamos useEffect para solo cuando se carge en componente y haya cambio en las dependencias(variables) counter y isClose se ejecute este codigo, si se realiza click fuera del componente se va a cerrar ya que contador pasa a valer 2
-    useEffect(()=>{
+    useEffect(() => {
         if (counter > 1) {
             isClose(false)
-            
-        }  
-    },[counter,isClose])
+
+        }
+    }, [counter, isClose])
 
 
     const amountToAdd = (number) => {
@@ -66,11 +66,11 @@ const ConfirmOrdersCart = ({ isOpenCart, isClose }) => {
     const item = <div className="confirm-orders-cart" onClick={handlerClickInsideCart}>
         {/* Dentro del componente se van a agregar la hamburguesas ordenadas */}
         <ul>
-        
-        {burgers && burgers.map((burger) => {return (<CartItem key={burger.id} src={burger.picture} alt={burger.name} itemName={burger.name} />)})}
+
+            {burgers && burgers.map((burger) => { return (<CartItem key={burger.id} src={burger.picture} alt={burger.name} itemName={burger.name} amount = {burger.quantity} id = {burger.id} />) })}
         </ul>
-        
-        
+
+
         {burgers.length > 0 && <aside>
             <div>
                 <h2>
@@ -102,12 +102,12 @@ const ConfirmOrdersCart = ({ isOpenCart, isClose }) => {
 
         </aside>
         }
-        {burgers.length > 0  && <Link to={'/order'}>
+        {burgers.length > 0 && <Link to={'/order'}>
             <button onClick={handlerToOpen}>Checkout</button>
         </Link>}
     </div>
 
-    
+
 
     return (
         //
