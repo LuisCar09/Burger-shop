fb81e54209427dc3bd1451670861a5c59c87a25d
Luis Carlos Bravo <luiscbravo94@gmail.com>
2024-03-06 15:59:01 +0000

    Added function to getBurger, and render data fetched from backend

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
 import burger1 from '../../assets/burger1.png'
 import burger2 from '../../assets/burger2.png'
 import burger3 from '../../assets/burger3.png'
-const Cart = () =>{
+import axios from "axios";
+
+
+const getBurgers = async () =>{
+    
+
+    try {
+        
+        const response = await axios.get('http://localhost:3001/add')
+        
+        return response.data
+    } catch (error) {
+        console.error('Error fetching data from server ',error.message);
+        return []
+    }
+}
+
+const Cart = () => {
+    const [burgers, setBurgers] = useState([])
+
+    const patchData = async (value, id) => {
+        console.log(value, id);
+
+        try {
+            if (value >= 1) {
+                axios.patch('http://localhost:3001/add', { value, id })
+            }
+        } catch (error) {
+            console.error('Error sending data to server ',error.message);
+        }
+    }
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const getData = await getBurgers()
+            console.log(getData);
+            setBurgers(getData)
+        }
+        fetchData()
+
+    }, [])
+
+
+
     return(
         <section className="cart-section">
             <div>
-
-                <CartItem src={burger1} alt={'Cheese Burger'} itemName={'Cheese Burger'} />
+            
+                {burgers && burgers.map((burger) => {return (<CartItem key={burger.id} src={burger.picture}  alt={burger.name} itemName={burger.name} amount={burger.quantity} id={burger.id} patchData ={patchData} />)})}
+                {/* <CartItem src={burger1} alt={'Cheese Burger'} itemName={'Cheese Burger'} />
                 <CartItem src={burger2} alt={'Veg Cheese Burger'} itemName={'Veg Cheese Burger'} />
 
-                <CartItem src={burger3} alt={'Cheese Burger with French Fries'} itemName={'Cheese Burger with French Fries'} />
+                <CartItem src={burger3} alt={'Cheese Burger with French Fries'} itemName={'Cheese Burger with French Fries'} /> */}
                 
 
                 <aside>
