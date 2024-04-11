d247237c223b84e465216f1f048d8c552276105d
Luis Carlos Bravo <luiscbravo94@gmail.com>
2024-04-11 14:52:50 +0100

    fixed bug at deleteData function, issue: at the momento to delete it does react like it should, e.g client delete an item then it delete but when client does not refresh page they cannot add more humburger because of this bug, at CartItem component added new a new component to show if the user wants to delete the item

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,30 +5,32 @@ import CartItem from "./CartItem";
 import axios from "axios";
 const API = 'http://localhost:3001/add'
 // fetching data to get burgers
-const getBurgers = async () =>{
+const getBurgers = async () => {
     try {
-        
+
         const response = await axios.get(API)
         return response.data
     } catch (error) {
-        console.error('Error fetching data from server ',error.message);
+        console.error('Error fetching data from server ', error.message);
         return []
     }
 }
-//Terminar de arreglar el componente delete para poder enviar los datos al backend y eliminar el elemente
-const Delete = ({showDeleteItem,deleteItem})=>{
-    
-    return(
+
+const Delete = ({ showDeleteItem, deleteItem }) => {
+
+    return (
         <article id="delete-pop-up">
             <div>
                 <div>
-                <p>Do you want to delete this item ?</p>
-                <button onClick={()=>{
-                    deleteItem(true)
-                    showDeleteItem(false)}}>Yes</button>
-                <button onClick={()=>{
-                    deleteItem(false)
-                    showDeleteItem(false)}} >No</button>
+                    <p>Do you want to delete this item ?</p>
+                    <button onClick={() => {
+                        deleteItem(true)
+                        showDeleteItem(false)
+                    }}>Yes</button>
+                    <button onClick={() => {
+                        deleteItem(false)
+                        showDeleteItem(false)
+                    }} >No</button>
                 </div>
             </div>
         </article>
@@ -37,99 +39,99 @@ const Delete = ({showDeleteItem,deleteItem})=>{
 
 const Cart = () => {
     const [burgers, setBurgers] = useState([])
-    const [showOrdersItems,setShowOrdersItems] = useState(false)
+    const [showOrdersItems, setShowOrdersItems] = useState(false)
     const [isShowWantToDeleteItem, setIsShowWantToDeleteItem] = useState(false)
-    const [deleteItem,setDeleteItem] = useState(false)
-    const [burgersHasChange,setBurgersHasChange] = useState(0);
-    const [amount,setAmount] = useState(0)
-    const [totalAmout,setTotalAmout] = useState({
-        tax:0,
-        shippingCharges:0,
-        total : 0,
+    const [deleteItem, setDeleteItem] = useState(false)
+    const [burgersHasChange, setBurgersHasChange] = useState(0);
+    const [amount, setAmount] = useState(0)
+    const [totalAmout, setTotalAmout] = useState({
+        tax: 0,
+        shippingCharges: 0,
+        total: 0,
     })
 
-    //Pracicamente podemos hacer un commit para guardar los cambios en este arreglo
+
     const patchData = async (value, id) => {
-        
+        console.log(`Linea 55, value: ${value} | id: ${id}`)
         try {
-            
+
             if (value >= 1) {
-                
-                axios.patch(API, { value, id })
+
+                await axios.patch(API, { value, id })
                 setBurgersHasChange(prevValue => prevValue + 1)
-            }else{
-                console.log('Value line 55',value)
-                setDeleteItem(true)
-                // axios.delete(API,{value,id})
             }
         } catch (error) {
-            console.error('Error sending data to server ',error.message);
+            console.error('Error sending data to server ', error.message);
         }
     }
-    //VOY AQUI DEBO ENVIAR LA DATA Y LUEGO ELIMINARLA SIEMPRE Y CUANDO EL USUARIO DIGA QUE SI
-    const deleteData = (value, id) => {
-        try {
-            console.log(`Value: ${value} id: ${id} line 71 `)
-            axios.delete(API, { value, id })
-        }
-        catch(error) {
-        console.log(error.message)
-        }
-}
-
-
-
     
+    const deleteData = async (id) => {
+
+        try {
+            console.log(`id: ${id} line 73`)
+            const detele = await axios.delete(`${API}/${id}`)
+            console.log(detele.data);
+            setBurgers(burgers.filter(burgers => burgers.id !== id))
+        }
+        catch (error) {
+            console.log(`Error sending data to serve ${error.message}`)
+        }
+    }
+
+
+
+
     useEffect(() => {
 
         const fetchData = async () => {
             const getData = await getBurgers();
             let amount = null
             setBurgers(getData)
-            
+
             if (getData.length >= 1) {
-                amount = getData.reduce((accumulator,burger)=>{
+                amount = getData.reduce((accumulator, burger) => {
                     return accumulator += burger.price * burger.quantity
-                },0)
-                
+                }, 0)
+
             }
             setAmount(amount)
         }
         fetchData()
 
     }, [burgersHasChange])
-   
-    useEffect(()=>{
-    
-    const tax = (amount * 0.07).toFixed(2);
-    const shippingCharges = amount > 18 ? 4 : 6;
-    const total = (amount + Number(tax) + shippingCharges).toFixed(2); 
-    
-    setTotalAmout((prevValue)=>{
-        return {
-            ...prevValue,
-            tax: tax,
-            shippingCharges : shippingCharges,
-            total : total
-        }
-    })
-   },[amount])
-   
-   //Show us not-order-yet section
-   useEffect(()=>{
-    burgers.length > 0 ? setShowOrdersItems(true) : setShowOrdersItems(false)
-   },[burgers])
 
-   
+    useEffect(() => {
+
+        const tax = (amount * 0.07).toFixed(2);
+        const shippingCharges = amount > 18 ? 4 : 6;
+        const total = (amount + Number(tax) + shippingCharges).toFixed(2);
+
+        setTotalAmout((prevValue) => {
+            return {
+                ...prevValue,
+                tax: tax,
+                shippingCharges: shippingCharges,
+                total: total
+            }
+        })
+    }, [amount])
+
+    //Show us not-order-yet section
+    useEffect(() => {
+        burgers.length > 0 ? setShowOrdersItems(true) : setShowOrdersItems(false)
+    }, [burgers])
+
+
 
     return (
 
         <section className="cart-section" >
-            {isShowWantToDeleteItem ? <Delete showDeleteItem={setIsShowWantToDeleteItem} deleteItem= {setDeleteItem} /> : null}
+            {isShowWantToDeleteItem ? <Delete showDeleteItem={setIsShowWantToDeleteItem} deleteItem={setDeleteItem} /> : null}
             {showOrdersItems && <div /*style={{display: !showOrdersItems? 'none': 'block'}}/>*/>
 
                 <article>
-                    {burgers && burgers.map((burger) => { return (<CartItem key={burger.id} src={burger.picture} alt={burger.name} itemName={burger.name} amount={burger.quantity} id={burger.id} patchData={patchData} deleteData={deleteData} setIsShowWantToDeleteItem={setIsShowWantToDeleteItem} />) })}
+                    {burgers && burgers.map((burger) => { return (<CartItem key={burger.id} src={burger.picture} alt={burger.name} itemName={burger.name} amount={burger.quantity} id={burger.id} patchData={patchData} deleteData={deleteData} deleteItem={deleteData} />) })}
+
                 </article>
 
 
@@ -166,10 +168,10 @@ const Cart = () => {
                     </div>
 
                     <div id="button-container">
-                    <button> <Link to={'/shipping'}>Checkout</Link> </button>
+                        <button> <Link to={'/shipping'}>Checkout</Link> </button>
                     </div>
                 </aside>
-                
+
             </div>}
             {showOrdersItems !== true ? <div id="not-order-yet">
                 <div>
@@ -178,7 +180,7 @@ const Cart = () => {
                         <Link to={'/menu'}><button>Go to menu</button></Link>
                     </div>
                 </div>
-            </div> : null} 
+            </div> : null}
 
         </section>
     )
--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,15 +1,35 @@
 
 import React, { useEffect, useRef, useState } from "react";
 
-const CartItem = ({ src, alt, itemName, amount, id,patchData,showDeleteItemWarning }) => {
+const Delete = ({setShowWantToDelete, setWantToDelete,setNumber})=>{
+    
+    return(
+        <div id="delete-pop-up">
+            <div>
+                <div>
+                <p>Do you want to delete this item ?</p>
+                <button onClick={()=>{
+                    setShowWantToDelete(false)
+                    setWantToDelete(true)
+                }}>Yes</button>
+                <button onClick={
+                    ()=>{
+                        setShowWantToDelete(false)
+                        setNumber(1)
+                    }}>No</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const CartItem = ({ src, alt, itemName, amount, id,patchData,deleteItem }) => {
 
     const [number, setNumber] = useState(0)
-    const [wantToDelete,setWantToDelete] = useState(false)
+    const [showWantToDelete,setShowWantToDelete] = useState(false)
+    const [wantToDelete,setWantToDelete] =useState(false)
     const prevNumberRef = useRef(number)
-    const showWarning = () =>{
-        
-        
-    }
+    
     
     //here we  add +1 every time (+) is clicked 
     const numberToAdd = async () => {
@@ -33,12 +53,20 @@ const CartItem = ({ src, alt, itemName, amount, id,patchData,showDeleteItemWarni
         //using useRef, we ref number and then we send data to backend and avoiding multiple rendenring issues
         newValue <= 0 ? prevNumberRef.current = 0 : prevNumberRef.current = newValue;
 
+
+        
         //here we send data to patchData function which lives in confirmOrdersCart. this function send data to backend to changes quantity of burger chosen or if newValue === 0 send data to delete
         
-        newValue > 0 ? patchData(prevNumberRef.current, id) : showDeleteItemWarning(prevNumberRef.current, id)
-        //  sendDataToPatch(prevNumberRef.current)  
-
-
+    
+        if (newValue > 0) {
+            patchData(prevNumberRef.current, id)
+        }else{
+            
+            setShowWantToDelete(true)
+            
+        }
+        
+        //  sendDataToPatch(prevNumberRef.current)   
     }
     //Change number value, this value comes from confirOrdersCart, we receive this value from backend with burger's amount, then put into number variable to set it up, then this value will render on each burger to says us how many burger were chosen.
     useEffect(() => {
@@ -54,6 +82,14 @@ const CartItem = ({ src, alt, itemName, amount, id,patchData,showDeleteItemWarni
     useEffect(() =>{
         return () => {prevNumberRef.current = null};
     },[] )
+
+    useEffect(()=>{
+        
+        if (wantToDelete) {
+            
+            deleteItem(id)
+        }
+    },[wantToDelete,deleteItem])
     return (
         <div>
             <div>
@@ -66,6 +102,7 @@ const CartItem = ({ src, alt, itemName, amount, id,patchData,showDeleteItemWarni
                 { <span>{number}</span> }
                 <button onClick={numberToAdd}>+</button>
             </div>
+            {showWantToDelete ? <Delete setShowWantToDelete={setShowWantToDelete} setWantToDelete={setWantToDelete} setNumber={setNumber} /> : <div style={{display:'none'}}></div>}
         </div>)
 }
 
