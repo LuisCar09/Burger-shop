34bb97bce1ce871add881990def0495bffc3f8c6
Luis Carlos <luiscbravo94@gmail.com>
2025-03-21 12:42:21 +0100

    feat: added dependencies features

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 import React, { useState } from "react";
 import textArea from "../text_area/TextArea";
 import Input from '../input/Input'
